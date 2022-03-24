5d6014846b2b22bda0e295f253c7ea18685d4d34
sumankandel12345 <sumankandel321@gmail.com>
2022-03-24 11:29:50 +0545

    himalayanjava

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+
+    extend: {
+     colors:{
+       matblack:"#f5eadd",
+       whitelight: "fef8f1",
+       graylight: "#fffcf9"
+     },
+    
+     fontFamily:{
+       samp:['Roboto', 'sans-serif'],
+       lato:['Lato', 'sans-serif'],
+       cursive:['Dancing Script', 'sans-serif']
+      
+     }
+     
+        
+    },
+  },
+  plugins: [],
+}
